6c11c261a0c3ef6d96f4d6377dfd341740f7ebcf
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-02-18 16:16:36 -0300

    refatorado o codigo para melhorar a legibilidade, utilizado tecnica DSL do kotlin para o consumo de uma mesma funcao facilitando a legibilidade e a criação dos fragments

--- app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import br.com.alura.technews.R
 import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.ui.activity.extensions.transacaoFragment
 import br.com.alura.technews.ui.fragment.ListaNoticiasFragment
 import br.com.alura.technews.ui.fragment.VisualizaNoticiaFragment
 
@@ -19,9 +21,13 @@ class NoticiasActivity: AppCompatActivity() {
         setContentView(R.layout.activity_noticias)
         title = TITULO_APPBAR
 
-        val transacao = supportFragmentManager.beginTransaction()
-        transacao.add(R.id.activity_noticias_container, ListaNoticiasFragment(), "lista-noticias")
-        transacao.commit()
+//        val transacao = supportFragmentManager.beginTransaction()
+//        transacao.add(R.id.activity_noticias_container, ListaNoticiasFragment(), "lista-noticias")
+//        transacao.commit()
+        //este codigo substitui o fonte acima
+        transacaoFragment {
+            add(R.id.activity_noticias_container, ListaNoticiasFragment(), "lista-noticias")
+        }
 
     }
 
@@ -30,20 +36,26 @@ class NoticiasActivity: AppCompatActivity() {
         super.onAttachFragment(fragment)
         //verifico qual o tipo de fragment q esta sendo utilizado,
         // lembrando que essa imoplementação permite uma flexibilidade maior no uso de fragments
-        if(fragment is ListaNoticiasFragment){
-            fragment.quandoNoticiaSelecionada = {
-                abreVisualizadorNoticia(it)
+        when(fragment){
+            is ListaNoticiasFragment -> {
+                configuraListaNoticias(fragment)
             }
 
-            fragment.quandoFabSalvaNoticiaClicada = {
-                abreFormularioModoCriacao()
+            is VisualizaNoticiaFragment -> {
+                configuraVisualizaNoticia(fragment)
             }
         }
+    }
 
-        if(fragment is VisualizaNoticiaFragment){
-            fragment.quandoFinalizaTela = { finish() }
-            fragment.quandoSelecionaMenuEdicao = { noticiaSelecionada -> abreFormularioEdicao(noticiaSelecionada) }
-        }
+    private fun configuraVisualizaNoticia(fragment: VisualizaNoticiaFragment) {
+        fragment.quandoFinalizaTela = { finish() }
+        fragment.quandoSelecionaMenuEdicao =
+            { noticiaSelecionada -> abreFormularioEdicao(noticiaSelecionada) }
+    }
+
+    private fun configuraListaNoticias(fragment: ListaNoticiasFragment) {
+        fragment.quandoNoticiaSelecionada = this::abreVisualizadorNoticia
+        fragment.quandoFabSalvaNoticiaClicada = this::abreFormularioModoCriacao
     }
 
     private fun abreFormularioModoCriacao() {
@@ -57,7 +69,7 @@ class NoticiasActivity: AppCompatActivity() {
 //        startActivity(intent)
 
         //criando um fragment programaticamente
-        val transacao = supportFragmentManager.beginTransaction()
+//        val transacao = supportFragmentManager.beginTransaction()
         val fragment = VisualizaNoticiaFragment()
         val dados = Bundle()
         dados.putLong(NOTICIA_ID_CHAVE, noticia.id)
@@ -69,11 +81,19 @@ class NoticiasActivity: AppCompatActivity() {
 //        }
 //        transacao.add(R.id.activity_visualiza_noticia_viewgroup_fragment_container,fragment)
         //esta linha de codigo equivale a remover o fragment conforme o comportamento programado acima
-        transacao.replace(R.id.activity_visualiza_noticia_viewgroup_fragment_container,fragment)
+//        transacao.replace(R.id.activity_visualiza_noticia_viewgroup_fragment_container,fragment)
         //executa a criacao do fragment
-        transacao.commit()
+//        transacao.commit()
+
+        //apenas esse metodo criado conseguimos absolver as linhas acima
+        transacaoFragment {
+            replace(R.id.activity_visualiza_noticia_viewgroup_fragment_container,fragment)
+
+        }
 
     }
+
+
 
     private fun abreFormularioEdicao(noticia: Noticia) {
         val intent = Intent(this, FormularioNoticiaActivity::class.java)
--- app/src/main/java/br/com/alura/technews/ui/activity/extensions/ActivityExtensions.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/extensions/ActivityExtensions.kt
@@ -2,6 +2,8 @@ package br.com.alura.technews.ui.activity.extensions
 
 import android.app.Activity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 fun Activity.mostraErro(mensagem: String) {
     Toast.makeText(
@@ -9,4 +11,12 @@ fun Activity.mostraErro(mensagem: String) {
         mensagem,
         Toast.LENGTH_LONG
     ).show()
+}
+
+//vamos fazer um metodo que recebe uma expressao lambda que sera executado pelo FragmentTransaction
+//utilizamos uma tecnica do kotlin chamada DSL
+fun AppCompatActivity.transacaoFragment(executa: FragmentTransaction.() -> Unit ){
+    val transaction = supportFragmentManager.beginTransaction()
+    executa(transaction)
+    transaction.commit()
 }
